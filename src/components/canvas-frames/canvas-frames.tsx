a88a5603cb145eed8c4838e9eab18f132e5ebde9
Patrick Eriksson <paleite@users.noreply.github.com>
2024-04-17 14:49:02 +0200

    Progress

--- src/components/canvas-frames/canvas-frames.tsx
+++ src/components/canvas-frames/canvas-frames.tsx
@@ -16,17 +16,26 @@ const images = Array.from({ length: frameCount }, (_, i) => i + offset).map(
   },
 );
 
+const getScreenHeights = (screenHeights: number) => ({
+  percentage: `${Math.max((screenHeights - 1) * 100, 0)}%`,
+  viewport: `${Math.max(screenHeights * 100, 0)}vh`,
+});
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 type CanvasFramesProps = {
   // videoUrl: string;
   // posterUrl: string;
-  // duration: number;
+  // TODO: Make this required instead of optional
+  duration?: number;
 };
 
 const CanvasFrames: React.FunctionComponent<
   PropsWithChildren<CanvasFramesProps>
-> = ({ children }) => {
+> = ({ children, duration = 3.5 }) => {
+  const heights = getScreenHeights(duration);
+
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
@@ -46,14 +55,17 @@ const CanvasFrames: React.FunctionComponent<
     const airpods = { frame: 0 };
 
     gsap.to(airpods, {
+      // trigger: canvasRef,
       frame: frameCount - 1,
       snap: "frame",
       ease: "none",
+      trigger: canvasRef,
       scrollTrigger: {
-        // trigger: canvas,
-        scrub: 0.5,
-        // start: "top top",
-        // end: "bottom bottom",
+        trigger: canvasRef.current,
+        scrub: 0.1,
+        start: "top top",
+        end: `bottom+=${heights.percentage} bottom`,
+        markers: !import.meta.env.PROD,
       },
       onUpdate: () => {
         render();
@@ -65,6 +77,7 @@ const CanvasFrames: React.FunctionComponent<
     }
 
     function render() {
+      console.log("rendering frame", airpods.frame);
       const imageToDraw = images[airpods.frame];
       if (!context || !canvas || !imageToDraw) {
         return;
@@ -73,21 +86,23 @@ const CanvasFrames: React.FunctionComponent<
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.drawImage(imageToDraw, 0, 0);
     }
-  }, []);
+  }, [heights.percentage]);
 
   return (
     <>
       <div
-        className="flex bg-black"
+        // className="flex bg-slate-500/50 overflow-hidden"
         data-testid="canvas-container"
         style={{
-          height: "5000px",
+          // height: "calc(" + (3.5 * 100 / frameCount) + " * 100vh)",
+          height: heights.viewport,
+          // height: "1050px",
         }}
       >
-        <div className="fixed flex h-screen w-screen bg-black">
+        <div className="fixed flex h-full w-full bg-amber-500/50">
           <canvas
             ref={canvasRef}
-            className="h-screen w-screen bg-black object-cover"
+            className="h-screen w-screen bg-red-500/30 object-cover"
           />
         </div>
       </div>
